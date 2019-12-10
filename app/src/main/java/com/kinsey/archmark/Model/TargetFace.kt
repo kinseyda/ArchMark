c0e593a25d6ef18d753885dec59036e1aad66981
Daniel Kinsey <daniel.kinsey@mail.utoronto.ca>
2019-12-10 18:16:19 -0500

    Convert TargetFace constructor to primary constructor

--- app/src/main/java/com/kinsey/archmark/Model/TargetFace.kt
+++ app/src/main/java/com/kinsey/archmark/Model/TargetFace.kt
@@ -1,16 +1,15 @@
 package com.kinsey.archmark.Model
 
-class TargetFace {
-
+class TargetFace(sizes: List<Float>, scores: List<Float>) {
+    //TODO change this to take in a dictionary or something, so that sizes and scores are always the same length
     var rings: MutableList<Ring> = mutableListOf()
 
-    constructor(sizes: List<Float>, scores: List<Float>) {
-        //TODO change this to take in a dictionary or something, so that sizes and scores are always the same length
-
+    init {
         for (i in sizes.indices) {
             rings.add(Ring(sizes[i], scores[i]))
         }
-
     }
+
+
 
 }
