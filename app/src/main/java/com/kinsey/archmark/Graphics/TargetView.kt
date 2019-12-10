90ccb3f689dfa008b8e1006c141917fa9583c4a2
Daniel Kinsey <daniel.kinsey@mail.utoronto.ca>
2019-12-10 17:55:44 -0500

    Make TargetView use RingDrawer

--- app/src/main/java/com/kinsey/archmark/Graphics/TargetView.kt
+++ app/src/main/java/com/kinsey/archmark/Graphics/TargetView.kt
@@ -2,22 +2,25 @@ package com.kinsey.archmark.Graphics
 
 import android.content.Context
 import android.graphics.Canvas
-import android.graphics.Color
-import android.graphics.Paint
 import android.util.AttributeSet
 import android.view.View
+import com.kinsey.archmark.Model.TargetFace
+import java.lang.Integer.min
 
-class TargetView @JvmOverloads constructor(context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0): View(context, attrs, defStyleAttr) {
-    private val paint =
-        Paint().apply {
-            isAntiAlias = true
-            color = Color.RED
-            style = Paint.Style.STROKE
-        }
+class TargetView @JvmOverloads constructor(context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0, private var targetFace: TargetFace): View(context, attrs, defStyleAttr) {
+    private var ringDrawer: RingDrawer = RingDrawer()
+
+    private var centerX = this.width/2
+    private var centerY = this.height/2
+    //TODO update these on view resize
 
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
-
-        canvas.drawCircle(3f, 3f, 3f, paint)
+        val diameter = min(this.width, this.height)
+        for (ring in this.targetFace.rings) {
+            this.ringDrawer.drawRing(canvas, ring, centerX, centerY, diameter)
+        }
     }
+
+
 }
