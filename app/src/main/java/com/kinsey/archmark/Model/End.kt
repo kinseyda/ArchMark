f93ea0bfc1235326f6f6bebc8add1a5e4855ebff
Daniel Kinsey <daniel.kinsey@mail.utoronto.ca>
2019-12-11 16:00:33 -0500

    Add End class

--- app/src/main/java/com/kinsey/archmark/Model/End.kt
+++ app/src/main/java/com/kinsey/archmark/Model/End.kt
@@ -1,2 +1,9 @@
 package com.kinsey.archmark.Model
 
+class End {
+    var arrows: MutableList<Arrow> = mutableListOf<Arrow>()
+
+    fun addArrow(arrow: Arrow) {
+        this.arrows.add(arrow)
+    }
+}
