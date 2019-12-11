cb31a6847ef9d4f4a109b4bca21589ed8008ff32
Daniel Kinsey <daniel.kinsey@mail.utoronto.ca>
2019-12-10 22:04:30 -0500

    Remove center vars in TargetView

--- app/src/main/java/com/kinsey/archmark/Graphics/TargetView.kt
+++ app/src/main/java/com/kinsey/archmark/Graphics/TargetView.kt
@@ -10,12 +10,10 @@ import java.lang.Integer.min
 class TargetView (context: Context, private var targetFace: TargetFace): View(context) {
     private var ringDrawer: RingDrawer = RingDrawer()
 
-    private var centerX = this.width/2
-    private var centerY = this.height/2
-    //TODO update these on view resize
-
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
+        var centerX = this.width/2
+        var centerY = this.height/2
         val radius = min(this.width, this.height)/2
         for (ring in this.targetFace.rings) {
             this.ringDrawer.drawRing(canvas, ring, centerX, centerY, radius)
