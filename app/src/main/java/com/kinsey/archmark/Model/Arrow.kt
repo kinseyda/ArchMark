37689729564edbd6cd53653a2c45fe0fd7c99e33
Daniel Kinsey <daniel.kinsey@mail.utoronto.ca>
2019-12-12 00:29:16 -0500

    Make Arrow note more specific

--- app/src/main/java/com/kinsey/archmark/Model/Arrow.kt
+++ app/src/main/java/com/kinsey/archmark/Model/Arrow.kt
@@ -1,7 +1,7 @@
 package com.kinsey.archmark.Model
 
 /**
- * Angle in radians from north, distance in cm
+ * Angle in radians from north (counter clockwise is positive, clockwise is negative), distance in cm
  */
 class Arrow(val angle: Float, val distance: Float) {
 
