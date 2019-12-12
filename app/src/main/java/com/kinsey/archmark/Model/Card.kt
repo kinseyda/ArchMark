5a2d20ea94d6f1e21b62720d00b6852bb5452b21
Daniel Kinsey <daniel.kinsey@mail.utoronto.ca>
2019-12-11 19:08:31 -0500

    Add Card class

--- app/src/main/java/com/kinsey/archmark/Model/Card.kt
+++ app/src/main/java/com/kinsey/archmark/Model/Card.kt
@@ -1,2 +1,10 @@
 package com.kinsey.archmark.Model
 
+class Card {
+    var ends: MutableList<End> = mutableListOf<End>()
+
+
+    fun addArrow(arrow: Arrow) {
+        this.ends.last().addArrow(arrow)
+    }
+}
