5a84a670c3b7db7243f7bb0b1c2b4c892e6e25c8
Daniel Kinsey <daniel.kinsey@mail.utoronto.ca>
2019-12-11 23:29:06 -0500

    Add initial End to Card in constructor

--- app/src/main/java/com/kinsey/archmark/Model/Card.kt
+++ app/src/main/java/com/kinsey/archmark/Model/Card.kt
@@ -2,7 +2,9 @@ package com.kinsey.archmark.Model
 
 class Card {
     var ends: MutableList<End> = mutableListOf<End>()
-
+    init{
+        ends.add(End())
+    }
 
     fun addArrow(arrow: Arrow) {
         this.ends.last().addArrow(arrow)
