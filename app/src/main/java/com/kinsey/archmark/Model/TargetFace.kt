4a89eb0a474f094b61462d16eb5d10cd158ed91b
Daniel Kinsey <daniel.kinsey@mail.utoronto.ca>
2019-12-11 23:29:27 -0500

    Add diameter parameter to TargetFace constructor

--- app/src/main/java/com/kinsey/archmark/Model/TargetFace.kt
+++ app/src/main/java/com/kinsey/archmark/Model/TargetFace.kt
@@ -1,6 +1,6 @@
 package com.kinsey.archmark.Model
 
-class TargetFace(sizes: List<Float>, scores: List<Float>) {
+class TargetFace(sizes: List<Float>, scores: List<Float>, val diameter: Float) {
     //TODO change this to take in a dictionary or something, so that sizes and scores are always the same length
     var rings: MutableList<Ring> = mutableListOf()
 
