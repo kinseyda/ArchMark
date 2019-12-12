cbb633b88c9826df7cad27a12e2ff2086e98311e
Daniel Kinsey <daniel.kinsey@mail.utoronto.ca>
2019-12-12 13:40:38 -0500

    Add method to find score/ring of Arrow

--- app/src/main/java/com/kinsey/archmark/Model/Arrow.kt
+++ app/src/main/java/com/kinsey/archmark/Model/Arrow.kt
@@ -3,6 +3,10 @@ package com.kinsey.archmark.Model
 /**
  * Angle in radians from north (counter clockwise is positive, clockwise is negative), distance in cm
  */
-class Arrow(val angle: Float, val distance: Float) {
+class Arrow(val angle: Float, val distance: Float, val targetFace: TargetFace, var forScore: Boolean = true) {
+
+    fun findRing(): Ring {
+        return this.targetFace.findRing(this)
+    }
 
 }
