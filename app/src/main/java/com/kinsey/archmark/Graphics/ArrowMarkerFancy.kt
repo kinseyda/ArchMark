72f9d3e6c39f6a830e130fecdfee08d5e748cfa1
Daniel Kinsey <daniel.kinsey@mail.utoronto.ca>
2019-12-14 12:20:40 -0500

    Add ArrowMarkerFancy
    This is a new type of ArrowMarker, which has vanes. Makes everything busier than anticipated, so maybe only useful when viewing the end rather than scoring it.

---
+++ app/src/main/java/com/kinsey/archmark/Graphics/ArrowMarkerFancy.kt
@@ -0,0 +1,58 @@
+package com.kinsey.archmark.Graphics
+
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
+import com.kinsey.archmark.Model.Arrow
+import java.lang.Math.PI
+import kotlin.math.cos
+import kotlin.math.sin
+
+class ArrowMarkerFancy(val arrow: Arrow, var targetView: TargetView) {
+
+    private val shaft = Paint().apply{
+        style = Paint.Style.FILL
+        color = Color.BLACK
+    }
+    private val shaftRadius = 20f
+
+    private val nock = Paint().apply{
+        style = Paint.Style.FILL
+        color = Color.WHITE
+    }
+    private val nockRadius = 15f
+
+    private val indexVane = Paint().apply{
+        style = Paint.Style.STROKE
+        strokeWidth = 5f
+        color = Color.WHITE
+    }
+    private val henVane = Paint().apply{
+        style = Paint.Style.STROKE
+        strokeWidth = 5f
+        color = Color.BLUE
+    }
+    private val vaneLength = 50f
+
+
+    fun drawMarker(canvas: Canvas) {
+        //Get coords
+        val cmCoords = cmPolarToCoordinates(arrow.angle, arrow.distance, targetView.targetFace)
+        val markerCoords = cmToPixel(cmCoords.first, cmCoords.second, targetView, targetView.targetFace)
+
+        //Draw index
+        canvas.drawLine(markerCoords.first, markerCoords.second, markerCoords.first, markerCoords.second - vaneLength, indexVane)
+
+        //Draw hen
+        canvas.drawLine(markerCoords.first, markerCoords.second, markerCoords.first + cos(PI / 5).toFloat()*vaneLength, markerCoords.second + sin(PI / 5).toFloat()*vaneLength , henVane)
+        canvas.drawLine(markerCoords.first, markerCoords.second, markerCoords.first - cos(PI / 5).toFloat()*vaneLength, markerCoords.second + sin(PI / 5).toFloat()*vaneLength , henVane)
+
+        //Draw shaft
+        canvas.drawCircle(markerCoords.first, markerCoords.second, this.shaftRadius, this.shaft)
+
+        //Draw nock
+        canvas.drawCircle(markerCoords.first, markerCoords.second, this.nockRadius, this.nock)
+
+    }
+
+}
