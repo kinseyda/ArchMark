90088410796bd03c8a38a0aeccad21898f934ef4
Daniel Kinsey <daniel.kinsey@mail.utoronto.ca>
2019-12-18 01:35:42 -0500

    Clean up ConversionUtils

--- app/src/main/java/com/kinsey/archmark/graphics/ConversionUtils.kt
+++ app/src/main/java/com/kinsey/archmark/graphics/ConversionUtils.kt
@@ -1,30 +1,60 @@
 package com.kinsey.archmark.graphics
 
+import androidx.arch.core.util.Function
 import com.kinsey.archmark.model.TargetFace
 import kotlin.math.*
 
 object ConversionUtils {
     fun pixelToCm(x: Float, y: Float, targetView: TargetView, targetFace: TargetFace): Pair<Float, Float> {
-        return Pair((((x - targetView.leftPadding)/targetView.paddedWidth)*targetFace.diameter), (((y - targetView.topPadding)/targetView.paddedHeight)*targetFace.diameter))
+        val xCm = getRelativeCmCoordinate(x, targetView.leftPadding, targetView.paddedWidth, targetFace.diameter)
+        val yCm = getRelativeCmCoordinate(y, targetView.topPadding, targetView.paddedHeight, targetFace.diameter)
+        return Pair(xCm, yCm)
+    }
+
+    private fun getRelativeCmCoordinate(coord: Float, padding: Int, paddedDimension: Int, diameter: Float): Float {
+        return ((coord - padding)/paddedDimension)*diameter
     }
 
     fun cmCoordinatesToPolar(x: Float, y: Float, targetFace: TargetFace): Pair<Float, Float> {
-        val radius = sqrt(((targetFace.diameter / 2) - x).toDouble().pow(2.0) + ((targetFace.diameter / 2) - y).toDouble().pow(2.0)).toFloat()
-        var angle = atan((((targetFace.diameter/2) - x)/((targetFace.diameter/2) - y)))
-        if (((targetFace.diameter/2) - y) < 0) {
-            angle += Math.PI.toFloat()
+        //Target radius offsets coordinates by radius in x and y directions, that's why the functions need them
+        val targetRadius = targetFace.diameter/2
+
+        val radius = getPolarRadius(targetRadius, x, y)
+        var angle = getPolarAngle(targetRadius, x, y)
+
+        //Account for CAST rule
+        if ((targetRadius - y) < 0) {
+            angle += PI.toFloat()
         }
         return Pair(angle, radius)
     }
 
+    private fun getPolarRadius(targetRadius: Float, relX: Float, relY: Float): Float {
+        return sqrt((targetRadius - relX).pow(2f) + (targetRadius - relY).pow(2f))
+    }
+
+    private fun getPolarAngle(targetRadius: Float, relX: Float, relY: Float): Float {
+        return atan((targetRadius-relX)/(targetRadius - relY))
+    }
+
     fun cmPolarToCoordinates(angle: Float, radius: Float, targetFace: TargetFace): Pair<Float, Float> {
-        val x = ((targetFace.diameter/2)- sin(angle)*radius)
-        val y = ((targetFace.diameter/2) - cos(angle)*radius)
+        fun getCoordinate(trig: (Float) -> Float): Float {
+            return ((targetFace.diameter/2)- trig(angle)*radius)
+        }
+        val x = getCoordinate(::sin)
+        val y = getCoordinate(::cos)
         return Pair(x, y)
     }
 
     fun cmToPixel(x: Float, y: Float, targetView: TargetView, targetFace: TargetFace): Pair<Float, Float> {
-        return Pair((((x /targetFace.diameter)*targetView.paddedWidth)+targetView.leftPadding), (((y /targetFace.diameter)*targetView.paddedHeight))+targetView.topPadding)
+        val xPx = getAbsolutePxCoordinate(x, targetFace.diameter, targetView.paddedHeight, targetView.leftPadding)
+        val yPx = getAbsolutePxCoordinate(y, targetFace.diameter, targetView.paddedWidth, targetView.rightPadding)
 
+        return Pair(xPx, yPx)
+
+    }
+
+    private fun getAbsolutePxCoordinate(coord: Float, diameter: Float, paddedDimension: Int, padding: Int): Float {
+        return ((coord /diameter)*paddedDimension)+padding
     }
 }
