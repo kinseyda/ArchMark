16a6b2547afaca96954e12255152c5bff4c159f6
Daniel Kinsey <daniel.kinsey@mail.utoronto.ca>
2019-12-18 01:15:31 -0500

    Fix references to ConversionUtils in ArrowMarkerFancy

--- app/src/main/java/com/kinsey/archmark/graphics/ArrowMarkerFancy.kt
+++ app/src/main/java/com/kinsey/archmark/graphics/ArrowMarkerFancy.kt
@@ -37,8 +37,8 @@ class ArrowMarkerFancy(val arrow: Arrow, var targetView: TargetView) {
 
     fun drawMarker(canvas: Canvas) {
         //Get coords
-        val cmCoords = cmPolarToCoordinates(arrow.angle, arrow.distance, targetView.targetFace)
-        val markerCoords = cmToPixel(cmCoords.first, cmCoords.second, targetView, targetView.targetFace)
+        val cmCoords = ConversionUtils.cmPolarToCoordinates(arrow.angle, arrow.distance, targetView.targetFace)
+        val markerCoords = ConversionUtils.cmToPixel(cmCoords.first, cmCoords.second, targetView, targetView.targetFace)
 
         //Draw index
         canvas.drawLine(markerCoords.first, markerCoords.second, markerCoords.first, markerCoords.second - vaneLength, indexVane)
