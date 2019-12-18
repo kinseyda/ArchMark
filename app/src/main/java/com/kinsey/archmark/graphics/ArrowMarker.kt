35b8c67f16b876d67c8604d62c159b0c3be8590d
Daniel Kinsey <daniel.kinsey@mail.utoronto.ca>
2019-12-18 01:19:14 -0500

    Move coordinate calculation from draw function to init of ArrowMarker

--- app/src/main/java/com/kinsey/archmark/graphics/ArrowMarker.kt
+++ app/src/main/java/com/kinsey/archmark/graphics/ArrowMarker.kt
@@ -6,6 +6,8 @@ import android.graphics.Paint
 import com.kinsey.archmark.model.Arrow
 
 class ArrowMarker(val arrow: Arrow, var targetView: TargetView) {
+    private val cmCoords = ConversionUtils.cmPolarToCoordinates(arrow.angle, arrow.distance, targetView.targetFace)
+    private val markerCoords = ConversionUtils.cmToPixel(cmCoords.first, cmCoords.second, targetView, targetView.targetFace)
 
     private val outer = Paint().apply{
         style = Paint.Style.FILL
@@ -20,11 +22,8 @@ class ArrowMarker(val arrow: Arrow, var targetView: TargetView) {
     private val innerRadius = 12f
 
     fun drawMarker(canvas: Canvas) {
-        val cmCoords = ConversionUtils.cmPolarToCoordinates(arrow.angle, arrow.distance, targetView.targetFace)
-        val markerCoords = ConversionUtils.cmToPixel(cmCoords.first, cmCoords.second, targetView, targetView.targetFace)
         canvas.drawCircle(markerCoords.first, markerCoords.second, this.outerRadius, this.outer)
         canvas.drawCircle(markerCoords.first, markerCoords.second, this.innerRadius, this.inner)
-
     }
 
 }
