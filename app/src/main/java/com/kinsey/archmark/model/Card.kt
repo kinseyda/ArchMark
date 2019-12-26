690758232126648be1034472b7e53ef4efe34545
Daniel Kinsey <daniel.kinsey@mail.utoronto.ca>
2019-12-26 11:49:58 -0500

    Add functions to Card for scorecard columns

--- app/src/main/java/com/kinsey/archmark/model/Card.kt
+++ app/src/main/java/com/kinsey/archmark/model/Card.kt
@@ -1,13 +1,17 @@
 package com.kinsey.archmark.model
 
+import java.util.*
+
 class Card {
     var ends: MutableList<End> = mutableListOf<End>()
     init{
         newEnd()
     }
 
-    fun cumulativeScore(): Float = ends.map { it.endTotal() }.sum()
+    fun cumulativeScore(): Float = cumulativeScore(ends.size-1)
 
+    fun cumulativeScore(upTo: Int): Float = ends.subList(0, upTo).map { it.endTotal() }.sum()
+    
     fun currentEnd(): End {
         return this.ends.last()
     }
@@ -18,5 +22,9 @@ class Card {
 
     fun addArrow(arrow: Arrow) {
         currentEnd().addArrow(arrow)
+    }
+
+    fun getMostArrows(): Int {
+        return Collections.max(ends.map { it.arrows.size })
     }
 }
