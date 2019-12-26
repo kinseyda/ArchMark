956f2670391c0ab79e98f15c140856450dfc6b0f
Daniel Kinsey <daniel.kinsey@mail.utoronto.ca>
2019-12-25 20:19:21 -0500

    Add code from main activity into relevant fragments

--- app/src/main/java/com/kinsey/archmark/graphics/TargetFragment.kt
+++ app/src/main/java/com/kinsey/archmark/graphics/TargetFragment.kt
@@ -1,20 +1,36 @@
 package com.kinsey.archmark.graphics
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.fragment.app.Fragment
 import com.kinsey.archmark.R
+import com.kinsey.archmark.model.Card
+import com.kinsey.archmark.model.TargetFace
 
-class TargetFragment: Fragment() {
+class TargetFragment(private val targetFace: TargetFace, private val card: Card): Fragment() {
+
+    lateinit var targetView: TargetView
+
+    lateinit var parentContext: Context
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.target_layout, container, false)
+        val parentView = inflater.inflate(R.layout.target_layout, container, false)
+        initView(parentView)
+        return parentView
+    }
+
+    fun initView(parentView: View) {
+        this.targetView = TargetView(this.parentContext, this.targetFace, this.card)
+        val constraintLayout: ConstraintLayout = parentView.findViewById(R.id.target_holder)
+        constraintLayout.addView(targetView)
     }
 
 }
