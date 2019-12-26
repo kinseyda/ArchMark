971ee0cd253c2d7ac6812b565028ac97a91f4664
Daniel Kinsey <daniel.kinsey@mail.utoronto.ca>
2019-12-26 18:53:13 -0500

    Add allArrows function to card

--- app/src/main/java/com/kinsey/archmark/model/Card.kt
+++ app/src/main/java/com/kinsey/archmark/model/Card.kt
@@ -13,6 +13,14 @@ class Card {
 
     fun cumulativeScore(upTo: Int): Float = ends.subList(0, upTo).map { it.endTotal() }.sum()
     
+    fun allArrows(): List<Arrow> {
+        val lst: MutableList<Arrow> = mutableListOf()
+        for (end in ends) {
+            lst += end.arrows
+        }
+        return lst
+    }
+    
     fun currentEnd(): End {
         return this.ends.last()
     }
