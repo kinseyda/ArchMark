845abb68c213d95b5b1a556b22a66b7a24c1b7d7
Daniel Kinsey <daniel.kinsey@mail.utoronto.ca>
2019-12-27 20:43:04 -0500

    Add basic save functionality
    Not including target face

--- app/src/main/java/com/kinsey/archmark/io/CardSaver.kt
+++ app/src/main/java/com/kinsey/archmark/io/CardSaver.kt
@@ -1,6 +1,8 @@
 package com.kinsey.archmark.io
 
+import com.kinsey.archmark.model.Arrow
 import com.kinsey.archmark.model.Card
+import com.kinsey.archmark.model.End
 import java.io.File
 
 /**
@@ -9,20 +11,52 @@ import java.io.File
  * - Time, number of arrows, notes and such (to be added at a later date)
  * Body is nested structure
  * - Every end in order, which is every arrow in chronological order
+ * Outside of body you have the TargetFace
+ * - A float diameter, then key-value pairs of ring sizes and scores
  */
 object CardSaver {
 
     fun saveCard(card: Card, file: File) {
-        var cardString = headString(card)
+        var cardString = "Card:\n"
+
+        cardString += headString(card)
+        cardString += bodyString(card)
 
         file.writeText(cardString)
     }
 
     private fun headString(card: Card): String {
-        var s = "Card:\n"
-        s += "\tTime: " + card.time.toString() + "\n"
-        s += "\tTotal: " + card.cumulativeScore() + "\n"
-        s += "\tArrows: " + card.allArrows().size + "\n"
+        var s = "\tHead:\n"
+        s += "\t\tTime: " + card.time.toString() + "\n"
+        s += "\t\tTotal: " + card.cumulativeScore() + "\n"
+        s += "\t\tArrows: " + card.allArrows().size + "\n"
+        return s
+    }
+
+    private fun bodyString(card: Card): String {
+        var s = "\tBody:\n"
+        for (end in card.ends) {
+            if (end.arrows.size == 0) {
+                continue
+            }
+            s += endString(end)
+        }
+        return s
+    }
+
+    private fun endString(end: End): String {
+        var s = "\t\tEnd:\n"
+        for (arrow in end.arrows) {
+            s += arrowString(arrow)
+        }
+        return s
+    }
+
+    private fun arrowString(arrow: Arrow): String {
+        var s = "\t\t\tArrow:\n"
+        s += "\t\t\t\tangle: " + arrow.angle + "\n"
+        s += "\t\t\t\tdistance: " + arrow.distance + "\n"
+        s += "\t\t\t\tforScore: " + arrow.forScore + "\n"
         return s
     }
 
