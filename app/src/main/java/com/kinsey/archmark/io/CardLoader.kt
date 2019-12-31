67b14a69b8e8564e4d8f5cc9fecdcd4921fefe8a
Daniel Kinsey <daniel.kinsey@mail.utoronto.ca>
2019-12-31 11:48:38 -0500

    Add base of Loading state machine

--- app/src/main/java/com/kinsey/archmark/io/CardLoader.kt
+++ app/src/main/java/com/kinsey/archmark/io/CardLoader.kt
@@ -1,11 +1,24 @@
 package com.kinsey.archmark.io
 
+import com.kinsey.archmark.model.Arrow
 import com.kinsey.archmark.model.Card
+import com.kinsey.archmark.model.End
 import java.io.File
 import java.io.IOException
+import java.io.StringBufferInputStream
 import java.util.*
 
 data class Head(var time: Long, var total: Float, var arrows: Int)
+
+enum class State {
+    //Names are what the line should be / what the machine is looking for
+    HEAD,
+    HEAD_TIME,
+    HEAD_TOTAL,
+    HEAD_ARROWS,
+
+    BODY
+}
 
 /**
  * Finite-State-Machine based approach to loading.
@@ -16,10 +29,33 @@ object CardLoader {
 
     //Regex patterns:
     //  Head:
-//    private val
+    private val rHead = Regex("^Head:$")
+    private val rHeadTime = Regex("^Time:[0-9]+$")
+    private val rHeadTotal = Regex("^Total:[0-9]+.[0-9]+$")
+    private val rHeadArrows = Regex("^Arrows:[0-9]+$")
 
     fun loadCard(file: File): Card {
+        var lineNum = 0
+        var state = State.HEAD
 
+        var card: Card? = null
+
+        file.forEachLine lineLoop@{
+            val line = it.replace(Regex("\\s"), "")
+            if (line.isEmpty()) {
+                return@lineLoop
+            }
+
+            when (state) {
+                State.HEAD ->
+                    if (rHead.matches(line)) state = State.HEAD_TIME
+                    else throw IOException(makeErrorString(state, lineNum))
+            }
+
+
+            println(line)
+            lineNum++
+        }
 
         return Card()
     }
@@ -28,6 +64,10 @@ object CardLoader {
 
 
         return Head(0, 0f,0)
+    }
+
+    private fun makeErrorString(state: State, lineNum: Int): String {
+        return ""
     }
 
 }
