0546e2c28b8fb3f298ed139d8d1e84f3d0d14604
Daniel Kinsey <daniel.kinsey@mail.utoronto.ca>
2020-02-04 23:09:05 -0500

    Change themeing

--- app/src/main/java/com/kinsey/archmark/MainActivity.kt
+++ app/src/main/java/com/kinsey/archmark/MainActivity.kt
@@ -90,6 +90,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setTheme(R.style.LightTheme)
         setContentView(R.layout.activity_main)
 
         this.setSupportActionBar(findViewById(R.id.toolbar))
