ec1f29793a5f40793d7bfb100992c6b40dc790ba
Daniel Kinsey <daniel.kinsey@mail.utoronto.ca>
2020-02-09 13:42:29 -0500

    Make Arrow location variables vars

--- app/src/main/java/com/kinsey/archmark/model/Arrow.kt
+++ app/src/main/java/com/kinsey/archmark/model/Arrow.kt
@@ -3,7 +3,7 @@ package com.kinsey.archmark.model
 /**
  * Angle in radians from north (counter clockwise is positive, clockwise is negative), distance in cm
  */
-class Arrow(val angle: Float, val distance: Float, val card: Card, var forScore: Boolean = true) {
+class Arrow(var angle: Float, var distance: Float, val card: Card, var forScore: Boolean = true) {
 
     fun findRing(): Ring? {
         return this.card.targetFace.findRing(this)
